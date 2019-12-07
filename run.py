8c5e933c0eaa74731d169eb74d94160d42829b24
R Sanjabi <rebecca.sanjabi@gmail.com>
2019-12-06 22:14:01 -0800

    grabbing list of works functional

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
 import works_list
 
-works_list.get_works_list ("Star Wars - All Media Types")
+works_list.get_works_list ("The Mandalorian (TV)", type_of='works', sort='date_posted', date_from='2019-12-05')
+#works_list.get_works_list ("Star Wars - All Media Types", type_of='works', sort='date_posted', date_from='2019-12-04')
--- works_list.py
+++ works_list.py
@@ -3,29 +3,102 @@ from bs4 import BeautifulSoup
 import progressbar
 import urllib.request
 import urllib.parse
+import math 
+import time
+import sys
 
-def gen_url(tag, type_of="works", sort='date_posted'):
+def initial_url(tag, type_of="works", sort='date_posted', date_from='all'):
     # generate url
-    #params = urllib.parse.urlencode({'spam': 1, 'eggs': 2, 'bacon': 0})
-    #url = "http://www.musi-cal.com/cgi-bin/query?%s" % params
-    #print(url)
+    ''' 
+    type_of = 'works' or 'bookmarks'
+    tag = string, tested on fandom name tags, but possibly any tags?
+    sort = 'date_posted' any other sort may not work???
+    date_from = defaults to all otherwise assume '2017-12-01' format December 1, 2017
+    '''
 
-    # parse URL to handle more stuff
-    # https://archiveofourown.org/works?utf8=%E2%9C%93&commit=Sort+and+Filter&work_search%5Bsort_column%5D=created_at&work_search%5Bother_tag_names%5D=&work_search%5Bexcluded_tag_names%5D=&work_search%5Bcrossover%5D=&work_search%5Bcomplete%5D=&work_search%5Bwords_from%5D=&work_search%5Bwords_to%5D=&work_search%5Bdate_from%5D=&work_search%5Bdate_to%5D=&work_search%5Bquery%5D=&work_search%5Blanguage_id%5D=&tag_id=Star+Wars+-+All+Media+Types
+    url_params = {'utf8': '✓', 
+                    'commit': 'Sort and Filter', 
+                    'work_search[sort_column]':'',
+                    'work_search[other_tag_names]': '', 
+                    'work_search[excluded_tag_names]': '',
+                    'work_search[crossover]': '',
+                    'work_search[complete]': '',
+                    'work_search[words_from]': '',
+                    'work_search[words_to]': '',
+                    'work_search[date_from]': '',
+                    'work_search[date_to]': '',
+                    'work_search[query]': '',
+                    'work_search[language_id]': '',
+                    'tag_id': tag,
+                    'page': ''
+                    }
+    if sort == 'date_posted':
+        url_params['work_search[sort_column]'] = 'created_at'
+    if date_from != 'all':
+        url_params['work_search[date_from]'] = date_from
+
+    params = urllib.parse.urlencode(url_params)
     
-    prefix = "https://archiveofourown.org/tags/"
-    searchterm = urllib.parse.quote(tag) +"/"
-    url = prefix + searchterm + type_of
-    print(url)
+    url = "https://archiveofourown.org/" + type_of + "?%s" % params
 
-    return url
+    return url, url_params
 
-def get_works_list(tag, type_of="works", sort='date_posted'):
-    ''' type = "bookmarks" '''
+def gen_urls(url_params, type_of, max_pages):
+    ''' Generate each subpage '''
 
-    base_url = gen_url(tag, type_of, sort)
+    for i in range(2, max_pages+1):
+        url_params['page'] = i
+        params = urllib.parse.urlencode(url_params)
+        url = "https://archiveofourown.org/" + type_of + "?%s" % params
+        yield url
+    return
+
+def get_works_list(tag, type_of, sort, date_from):
+
+
+    base_url, url_params = initial_url(tag, type_of, sort, date_from)
+
+    print("Searching for work id's starting on page 1 here: ", base_url)
+
     with urllib.request.urlopen(base_url) as f:
-        print(f.read().decode('utf-8'))
-    # iterate through each page
-    # scrape urls
-    # write urls to text file
+        soup = BeautifulSoup(f.read().decode('utf-8'), features="lxml")
+    work_blurb_groups = soup.find_all(class_="work blurb group")
+    with open('works_list.txt', "w") as out:
+            out.write(base_url+"\n")
+    for work in work_blurb_groups:
+        with open('works_list.txt', "a") as out:
+            a = work.find('a').get('href').lstrip('/works/')
+            out.write(a+"\n")
+    
+    # Find max number of pages
+    try:
+        heading = soup.find('h2')
+        count = heading.get_text().split(' Works in ')[0].strip()
+        if (count.isnumeric()):
+            max_pages = 1
+        else:
+            num_works = int(count.split(' of ')[1])
+            max_pages = math.ceil(num_works/20)
+    except:
+        print("Error trying to compute number of pages to scrape.")
+        sys.exit()
+
+    bar = progressbar.ProgressBar(max_value=max_pages)
+    bar.update(1)
+    pages = gen_urls(url_params, type_of, max_pages)
+
+    for i, page in enumerate(pages):
+        time.sleep(1)
+        bar.update(i+1)
+
+        with urllib.request.urlopen(page) as f:
+            soup = BeautifulSoup(f.read().decode('utf-8'), features="lxml")
+        work_blurb_groups = soup.find_all(class_="work blurb group")
+        for work in work_blurb_groups:
+            with open('works_list.txt', "a") as out:
+                x = work.find('a').get('href').lstrip('/works/')
+                out.write(x+"\n")
+
+    return
+
+
