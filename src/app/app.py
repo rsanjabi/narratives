0d4d888d826da6722b579449d6cabfa144cf8726
R Sanjabi <rebecca.sanjabi@gmail.com>
2020-03-01 10:12:02 -0800

    basic front end functionality

--- src/app/app.py
+++ src/app/app.py
@@ -1,40 +1,24 @@
 
 from flask import Flask, request, jsonify, render_template
 import pickle
-import numpy as np
-from lightfm import LightFM
+import implicit
+from markupsafe import Markup
 
 app = Flask(__name__)
-model = pickle.load(open('../../data/models/test.pkl', 'rb'))
 
+NUM_TO_RETURN = 5
 
+@app.before_first_request
+def load__model():
+    global model
+    global indices
+    global inverted_indices
 
-def sample_fanworks_rec(model, dataset, users):
-
-    n_users, n_items = dataset.interactions_shape()
-    
-    for user_name in users:
-        #known_positives = data['item_labels'][data['train'].tocsr()[user_id].indices]
-        user_id = dataset.mapping()[0][user_name]
-        
-        scores2 = model.predict(user_id, np.arange(n_items))
-        #top_items = data['work_id'][np.argsort(-scores)]
-        top_indices = np.argsort(-scores2)
-        #top_items = dataset.mapping()[2][np.argsort(-scores)]
-
-        print(f"User {user_id} {user_name}")
-        print("     Known positives:")
-        
-
-        #for x in known_positives[:3]:
-        #    print("        %s" % x)
-
-        print("     Recommended:")
-
-        for x in top_indices[:10]:
-
-            y = dict(map(reversed, dataset.mapping()[2].items()))[x]
-            print(f"http://www.archiveofourown.org/works/{y}")
+    model = pickle.load(open('../../models/bpr270220.pkl', 'rb'))
+    indices = pickle.load(open('../../models/indices270220.pkl', 'rb'))
+    inverted_indices = {'work_id':{}, 'user':{}}
+    inverted_indices['work_id'] = {v: k for k, v in indices['work_id'].items()}
+    inverted_indices['user'] = {v: k for k, v in indices['user'].items()}
 
 @app.route('/')
 def home():
@@ -42,20 +26,20 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    fanwork_id = [int(x) for x in request.form.values()][0]
-    scores2 = model.predict(fanwork_id, np.arange(4224))
-    top_indices = np.argsort(-scores2)
-    return render_template('index.html', prediction_text='Getting predictions for {}'.format(top_indices[0]))
-
-
-@app.route('/results',methods=['POST'])
-def results():
-
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
-
-    output = prediction[0]
-    return jsonify(output)
+    if request.method == 'POST':
+        results = ""
+        prefix = "<a href='http://www.archiveofourown.org/works/"
+        postfix = "'>"
+        postpostfix = "</a>\n"
+        fanwork_id = [int(x) for x in request.form.values()][0]
+        work_indice = indices['work_id'][str(fanwork_id)]
+        related_BPR = model.similar_items(work_indice, NUM_TO_RETURN)
+        for suggestion in related_BPR:
+            work_id = inverted_indices['work_id'][suggestion[0]]
+            results = results + prefix + work_id + postfix + work_id + postpostfix
+        return render_template('index.html', prediction_text = Markup(results))
+        #return render_template('index.html', results
+    #return render_template('index.html', prediction_text=results)
 
 if __name__ == "__main__":
     app.run(debug=True)
