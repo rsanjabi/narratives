9e695c4d7c6998206b622f7338339590793461d9
R Sanjabi <rebecca.sanjabi@gmail.com>
2020-04-07 21:50:44 -0700

    get_kudos works with config and paths

--- src/run.py
+++ src/run.py
@@ -2,8 +2,9 @@
 ''' Short term code to create a pipeline. Will be replaced by Prefect or Airflow in the future '''
 
 import scrape.ao3_get_meta as meta
-#import src.scrape.ao3_get_kudos as kudos
+import scrape.ao3_get_kudos as kudos
 import config as cfg
 
 
-meta.scrape(cfg.TEST_FANDOM, 'meta.csv', True, False)
+#meta.scrape(cfg.TEST_FANDOM, 'meta.csv', True, False)
+kudos.scrape(cfg.TEST_FANDOM, 'meta.csv', 'kudos.csv', '')
--- src/scrape/ao3_get_kudos.py
+++ src/scrape/ao3_get_kudos.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """Scraping tool to get a list of kudos for a list of fanworks.
 
-Set config options such as the time delay for each http request in config.py
+Set config options such as the time DELAY for each http request in config.py
 
 Output is in csv format of <fandom_id>,<named_kudo_giver>
 
@@ -13,11 +13,13 @@ Example URL to be scraped:
 
 Example:
 
-    scrap.ao3_get_kudos(in_csv_file, out_csv_file, restart, header_string)
+    scrap.ao3_get_kudos(in_csv_file, out_csv_file, restart)
 
 TODO:
-    * Refactor to ensure all module level variables synced with config.py
-    * Remove get_args functionality
+    * Remove get_args()
+    * Do paths work?
+    * Restart working?
+    * access_denied() is a stub
 
 """
 
@@ -28,23 +30,29 @@ import sys
 import argparse
 import requests
 from bs4 import BeautifulSoup
+from pathvalidate import replace_symbol
+import config as cfg
+
+DELAY = cfg.DELAY
+RAW_PATH = cfg.RAW_PATH
+HTTP_HEAD = cfg.HTTP_HEADERS
 
 def get_args():
     """Converts command line arguments into variables."""
 
     parser = argparse.ArgumentParser(description='Scrape AO3 work IDs to find users who gave kudos')
-    parser.add_argument('--in_csv', default='fanworks_ids.csv', help='csv input file name')
-    parser.add_argument('--out_csv', default='fanworks_kudos.csv', help='csv output file name')
+    parser.add_argument('--fandom', default='', help='fandom name')
+    parser.add_argument('--in_csv', default='meta.csv', help='csv input file name')
+    parser.add_argument('--out_csv', default='kudos.csv', help='csv output file name')
     parser.add_argument('--restart', default='', help='work_id to start at from within a csv')
-    parser.add_argument('--header', default='', help='user http header')
 
     args = parser.parse_args()
+    fandom = str(args.fandom)
     output_file = str(args.out_csv)
     input_file = str(args.in_csv)
     restart = str(args.restart)
-    headers = str(args.header)
 
-    return input_file, output_file, restart, headers
+    return fandom, input_file, output_file, restart
 
 def process_id(work_id, restart, found):
     """ Not found and not restart """
@@ -64,7 +72,7 @@ def access_denied(soup):
         pass
     return False
 
-def write_kudo_to_csv(work_id, writer, errorwriter, header_info=''):
+def write_kudo_to_csv(work_id, writer, errorwriter):
     '''
     work_id is the AO3 ID of a work
     writer is a csv writer object
@@ -75,9 +83,8 @@ def write_kudo_to_csv(work_id, writer, errorwriter, header_info=''):
     print('Scraping ', work_id)
 
     url = 'http://archiveofourown.org/works/'+str(work_id)+'/kudos'
-    headers = {'user-agent' : header_info}
 
-    req = requests.get(url, headers=headers)
+    req = requests.get(url, headers=HTTP_HEAD)
     if req.status_code != 200:
         print('Access Denied')
         error_row = [work_id] + ['Access Denied']
@@ -108,12 +115,14 @@ def write_kudo_to_csv(work_id, writer, errorwriter, header_info=''):
                 errorwriter.writerow(error_row)
         print('Done.')
 
-def main():
-    """ Open file and do the scraping """
+def scrape(fandom, csv_in, csv_out, restart):
+    """ Scrape the kudos from a list of fanwork_ids """
 
-    csv_in, csv_out, restart, headers = get_args()
-    delay = 5
-    os.chdir(os.getcwd())
+    fandom_dir = replace_symbol(fandom)
+    data_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), RAW_PATH+fandom_dir)
+    if not os.path.exists(data_path):
+        os.makedirs(data_path)
+    os.chdir(data_path)
     with open(csv_out, 'a') as f_out:
         writer = csv.writer(f_out)
         with open("errors_" + csv_out, 'a') as e_out:
@@ -126,11 +135,11 @@ def main():
             with open(csv_in, 'r+') as f_in:
                 reader = csv.reader(f_in)
                 if restart is '':
-                    for row in reader:
-                        if not row:
+                    for i, row in enumerate(reader):
+                        if i == 0:      # Skip header row
                             continue
-                        write_kudo_to_csv(row[0], writer, errorwriter, headers)
-                        time.sleep(delay)
+                        write_kudo_to_csv(row[0], writer, errorwriter)
+                        time.sleep(DELAY)
                 else:
                     found_restart = False
                     for row in reader:
@@ -138,10 +147,15 @@ def main():
                             continue
                         found_restart = process_id(row[0], restart, found_restart)
                         if found_restart:
-                            write_kudo_to_csv(row[0], writer, errorwriter, headers)
-                            time.sleep(delay)
+                            write_kudo_to_csv(row[0], writer, errorwriter)
+                            time.sleep(DELAY)
                         else:
                             print('Skipping already processed work')
+
+def main():
+    """ Open file and do the scraping """
+    fandom, csv_in, csv_out, restart = get_args()
+    scrape(fandom, csv_in, csv_out, restart)
 
 if __name__ == "__main__":
     main()
