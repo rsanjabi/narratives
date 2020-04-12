3703467fae0322f0c542fd2eaab60cacbb779ead
R Sanjabi <rebecca.sanjabi@gmail.com>
2020-04-11 23:18:51 -0700

    meta logging

--- src/run.py
+++ src/run.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
-''' Short term code to create a pipeline. Will be replaced by Prefect or Airflow in the future '''
+'''
+    Short term code to create a pipeline. Will be replaced by Prefect or
+    Airflow in the future.
+'''
 
 import scrape.ao3_get_meta as meta
-import scrape.ao3_get_kudos as kudos
+# import scrape.ao3_get_kudos as kudos
 import config as cfg
 
 
-meta.scrape(cfg.TEST_FANDOM, 'meta.csv', True, False)
-kudos.scrape(cfg.TEST_FANDOM, 'meta.csv', 'kudos.csv', '')
+meta.scrape(cfg.TEST_FANDOM, from_the_top=False)
+# kudos.scrape(cfg.TEST_FANDOM, 'meta.csv', 'kudos.csv', '')
+print(f"finished scraping meta and kudos for : {cfg.TEST_FANDOM}")
