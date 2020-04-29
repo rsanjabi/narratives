7896b368c3558d64ba29acc2845a0b7968752e78
R Sanjabi <rebecca.sanjabi@gmail.com>
2020-04-28 20:39:43 -0700

    linting fixes

--- tests/unit/test_meta.py
+++ tests/unit/test_meta.py
@@ -16,8 +16,9 @@ class TestMeta(unittest.TestCase):
         pass
 
     def setUp(self):
-        url = 'https://archiveofourown.org/tags/Star%20Wars%20-%20All%20Media%20Types/works'
-        TestMeta.bs = BeautifulSoup(urlopen(url), 'html.parser')
+        url = 'https://archiveofourown.org/tags/'
+        postfix = 'Star%20Wars%20-%20All%20Media%20Types/works'
+        TestMeta.bs = BeautifulSoup(urlopen(url+postfix), 'html.parser')
 
     def tearDown(self):
         pass
@@ -25,13 +26,14 @@ class TestMeta(unittest.TestCase):
     def test_titleText(self):
         pageTitle = TestMeta.bs.find('h1').get_text()
         self.assertEqual('\nArchive of Our Own beta\n', pageTitle)
-   
+
     def test_contentExists(self):
         content = TestMeta.bs.find('div')
         self.assertIsNotNone(content)
 
     def test_get_tag_info(self):
         pass
+
 
 if __name__ == "__main__":
     unittest.main()
