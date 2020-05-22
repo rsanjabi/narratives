abea494a7ac75fd16ec9e5655a84db7ad146755f
R Sanjabi <rebecca.sanjabi@gmail.com>
2020-05-21 17:17:22 -0700

    first pass of kudo logic reorg

--- src/scrape/algo.py
+++ src/scrape/algo.py
@@ -5,13 +5,14 @@
 
 class FanworksBatch():
 
-    def __init__(self):
-        pass
+    def __init__(self, batch_size: int = 500):
+        self.batch_size = batch_size
 
     def compute(self):
         pass
 
     def next_batch(self):
+        # returns a list of work_ids
         pass
 
 
--- src/scrape/kudos.py
+++ src/scrape/kudos.py
@@ -2,7 +2,7 @@
 import time
 from datetime import datetime, timedelta
 import json
-from typing import Generator, Tuple, List
+from typing import List
 from mypy_extensions import TypedDict
 
 from bs4 import BeautifulSoup
@@ -12,6 +12,7 @@ from scrape.page import Page
 import utils.paths as paths
 import config as cfg
 from db.kudos_db import DBKudos     # type: ignore
+from scrape.algo import FanworksBatch
 
 KudosJson = TypedDict('KudosJson', {
                      'work_id': str,
@@ -21,75 +22,59 @@ KudosJson = TypedDict('KudosJson', {
 
 class Kudos(Page):
 
-    def __init__(self, fandom: str, from_top: bool = True):
-        self.log_path = paths.kudo_log_path(fandom)
-        self.kudo_path = paths.kudo_path(fandom)
-        self.input_path = paths.meta_path(fandom)
+    def __init__(self, num_batches: int = 1):
+        self.num_batches = num_batches
+        # TODO fix this inheritance issue (fandom, from top)
+        fandom = 'Star Wars'
+        self.log_path = paths.kudo_log_path('Star Wars')
+        self.kudo_path = paths.kudo_path('Star Wars')
         url = ('https://archiveofourown.org/works/')
-        super().__init__(fandom, 'kudos',
-                         self.log_path,
-                         self.kudo_path,
-                         url,
-                         from_top)
+        super().__init__(fandom, 'kudos', url, True)
 
-    def _pages(self) -> Generator[Tuple[BeautifulSoup, str], None, None]:
+    def scrape(self) -> None:
 
-        with open(self.input_path, 'r') as f_in:
+        kudo_batch = FanworksBatch(batch_size=10)
+        work_ids = kudo_batch.next_batch()
+        with open(self.kudo_path, 'w') as f_out:
+            for id in work_ids:
+                page = self.get_page(id)
+                kudos = self._page_elements(page, id)
+                f_out.write(json.dumps(kudos)+'\n')
+                self.progress.write(id)
+        self.logger.info(f'Completed scraping "{self.page_kind}"')
+        return
 
-            # encountered means we need to start scrape from beginning of file
-            # not encountered means we need to check when to start scraping
-            if (self.from_top is True
-                    or self.last == self.progress.unscraped_flag):
-                encountered = True
+    def _get_page(self, page: BeautifulSoup, work_id: str) -> BeautifulSoup:
+
+        url = self.base_url + work_id + '/kudos'
+        errors = 0
+
+        while errors < cfg.MAX_ERRORS:
+            try:
+                time.sleep(cfg.DELAY)
+                soup = self._get_soup(url)
+                self.logger.info(f"Scraped id: {work_id}")
+            except HTTPError:
+                self.logger.info(f"HTTPError: {work_id}")
+                errors += 1
+                time.sleep(cfg.DELAY*errors*10)    # Increase sleep time
+            # except 404 error:
+            #   print work_id to tbdeleted log
+            except ConnectTimeout:
+                self.logger.error(f"ConnectionTimeout on: {work_id}")
+                self.logger.error(f"{cfg.MAX_ERRORS-errors} errors left.")
+                errors += 1
+                time.sleep(cfg.DELAY*errors*10)    # Increase sleep time
             else:
-                encountered = False
-            errors = 0
+                return soup
+        return None
 
-            for row_str in f_in:
-                row = json.loads(row_str)
-
-                if not encountered:
-                    # as soon as we find where to pick up, encountered = True
-                    if row['work_id'] == self.last:
-                        encountered = True
-                    continue
-
-                url = self.base_url + row['work_id'] + '/kudos'
-                self.fandom_id = row['work_id']
-
-                # Don't waste time rescraping if kudos were recently scraped
-                if self._recently_updated(row['work_id']):
-                    self.logger.info(
-                        f"Scraped recently. Skipping: {self.fandom_id}")
-                    continue
-
-                try:
-                    soup = self._get_soup(url)
-                    self.logger.info(f"Scraped id: {self.fandom_id}")
-                except HTTPError:
-                    self.logger.info(f"HTTPError/skipping: {self.fandom_id}")
-                except ConnectTimeout:
-                    self.logger.error(f"ConnectionTimeout "
-                                      f"on: {self.fandom_id}")
-                    if errors > cfg.MAX_ERRORS:
-                        self.logger.error(f"{cfg.MAX_ERRORS} "
-                                          f"encountered. Aborting.")
-                        return
-                    self.logger.error(f"{cfg.MAX_ERRORS-errors} errors left.")
-                    errors += 1
-                    time.sleep(cfg.DELAY*errors*10)    # Increase sleep time
-                else:
-                    yield soup, self.fandom_id
-                finally:
-                    time.sleep(cfg.DELAY)
-
-    def _page_elements(self, soup: BeautifulSoup) -> Generator[KudosJson,
-                                                               None, None]:
+    def _page_elements(self, soup: BeautifulSoup, id: str) -> KudosJson:
         k_d: KudosJson = {}       # type: ignore
-        k_d['work_id'] = self.fandom_id
+        k_d['work_id'] = id
         k_d['kudos'] = [x.text for x in soup.find(id="kudos").find_all('a')]
         k_d['scrape_date'] = datetime.now().strftime("%d/%b/%Y %H:%M")
-        yield k_d
+        return k_d
 
     def _recently_updated(self, work_id: str) -> bool:
         kudo_db = DBKudos(self.page_kind)
